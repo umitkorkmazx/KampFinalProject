737013fd17ab8ada0f946bd394ff33ce6dce5712
umitkorkmazx <umitkorkmazticaret@gmail.com>
2022-07-20 06:50:16 +0300

    8.Gün Sonu EntityFramework

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,19 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //open closed principle ** yaptığın yazılıma yeni bir özellik ekliyorsan
+    // mevucttaki hiç bir koduna dokunamazsın
     internal class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(20,46))
             {
                 Console.WriteLine(product.ProductName + "---=>" + product.UnitPrice);
             }
