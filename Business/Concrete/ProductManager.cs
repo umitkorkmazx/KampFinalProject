3762b62540939b5547c75b0517caa5fc379ba44b
umitkorkmazx <umitkorkmazticaret@gmail.com>
2022-07-27 06:00:13 +0300

    11. Gün Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -36,7 +36,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 05)
+            if (DateTime.Now.Hour == 07)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
