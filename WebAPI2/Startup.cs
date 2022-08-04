5a0298150353b4eb2bbe62f72eea526848746f10
umitkorkmazx <umitkorkmazticaret@gmail.com>
2022-08-04 07:02:58 +0300

    Startup frontend bağlantısı

--- WebAPI2/Startup.cs
+++ WebAPI2/Startup.cs
@@ -43,6 +43,8 @@ namespace WebAPI
             //services.AddSingleton<IProductService, ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
 
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -80,7 +82,7 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI2 v1"));
             }
-
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
 
             app.UseRouting();
